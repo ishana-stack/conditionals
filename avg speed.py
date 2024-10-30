58467d83ad7d3514b525150d4862e2baec6707ec
ishana-stack <cool.eshadeb@gmail.com>
2024-10-29 22:59:14 -0700

    Add files via upload

---
+++ avg speed.py
@@ -0,0 +1,7 @@
+Python 2.7.4 (default, Apr  6 2013, 19:54:46) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+20
+speed1 is less than avg
+>>> 
